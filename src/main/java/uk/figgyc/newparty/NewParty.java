d88a4f0e0df443513632d8379603d8193899fd2c
figgyc <figgycity@gmail.com>
2017-09-24 17:37:16 +0100

    bump version

--- src/main/java/uk/figgyc/newparty/NewParty.java
+++ src/main/java/uk/figgyc/newparty/NewParty.java
@@ -9,7 +9,7 @@ import net.minecraftforge.fml.common.event.FMLInitializationEvent;
 public class NewParty
 {
     public static final String MODID = "newparty";
-    public static final String VERSION = "1.0";
+    public static final String VERSION = "1.0.1";
     
     @EventHandler
     public void init(FMLInitializationEvent event)
